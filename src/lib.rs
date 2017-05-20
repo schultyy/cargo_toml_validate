722f58ec03b10ffedffc6172857351869f288b36
Jan Schulte <hello@unexpected-co.de>
2017-05-20 22:06:40 +0200

    feat: Perform checks for each field

--- src/lib.rs
+++ src/lib.rs
@@ -33,10 +33,25 @@ pub fn validate(cargo_toml: &str) -> Result<(), Vec<String>> {
                             Key::new_with_optional_name("license".into(), "license-file".into()),
                             Key::new("documentation".into()),
                             Key::new_with_optional_name("homepage".into(), "repository".into()));
-    Err(vec!("Description is missing".into(),
-                "license or license-file is missing".into(),
-                "documentation is missing".into(),
-                "homepage or repository are missing".into()))
+    let package_section = toml_table["package"].as_table().unwrap();
+
+    let mut errors = vec!();
+
+    if package_section.get("description").is_none() {
+        errors.push("Description is missing".into());
+    }
+
+    if package_section.get("license").is_none() {
+        errors.push("license or license-file is missing".into());
+    }
+
+    if package_section.get("homepage").is_none() {
+        if package_section.get("repository").is_none() {
+            errors.push("homepage or repository are missing".into());
+        }
+    }
+
+    Err(errors)
 }
 
 #[cfg(test)]
@@ -65,16 +80,72 @@ autho hulte <hello@unexpected-co.de>\"]
 ".into()
     }
 
+
+    fn description_is_missing() -> String {
+"
+[package]
+name = \"cargo_toml_validate\"
+version = \"2.0.0\"
+authors = [\"Jan Schulte <hello@unexpected-co.de>\"]
+license = \"MIT\"
+repository = \"https://github.com/schultyy/cargo_toml_validate\"
+".into()
+    }
+
+    fn repository_is_missing() -> String {
+"
+[package]
+name = \"cargo_toml_validate\"
+version = \"2.0.0\"
+authors = [\"Jan Schulte <hello@unexpected-co.de>\"]
+license = \"MIT\"
+description = \"This and that\"
+".into()
+    }
+
+    fn license_is_missing() -> String {
+"
+[package]
+name = \"cargo_toml_validate\"
+version = \"2.0.0\"
+authors = [\"Jan Schulte <hello@unexpected-co.de>\"]
+repository = \"https://github.com/schultyy/cargo_toml_validate\"
+description = \"This and that\"
+".into()
+    }
+
     #[test]
     fn it_should_fail_with_errors() {
         let results = validate(&invalid_cargo_toml());
         assert!(results.is_err());
-        assert_eq!(4, results.unwrap_err().len());
+        assert_eq!(3, results.unwrap_err().len());
     }
 
     #[test]
     fn it_should_fail_when_cargo_toml_is_malformed() {
         let results = validate(&malformed_cargo_toml());
+        assert!(results.is_err());
+        assert_eq!(1, results.unwrap_err().len());
+    }
+
+    #[test]
+    fn it_should_fail_when_description_is_missing() {
+        let results = validate(&description_is_missing());
+        assert!(results.is_err());
+        assert_eq!(1, results.unwrap_err().len());
+    }
+
+    #[test]
+    fn it_should_fail_when_license_is_missing() {
+        let results = validate(&license_is_missing());
+        assert!(results.is_err());
+        println!("{:?}", results);
+        assert_eq!(1, results.unwrap_err().len());
+    }
+
+    #[test]
+    fn it_should_fail_when_repository_is_missing() {
+        let results = validate(&repository_is_missing());
         assert!(results.is_err());
         assert_eq!(1, results.unwrap_err().len());
     }
