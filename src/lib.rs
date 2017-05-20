4215f1f82b655d6f2467bdf8a00f0abe58fcdb8e
Jan Schulte <hello@unexpected-co.de>
2017-05-20 22:11:13 +0200

    Chore: Get rid of unused code

--- src/lib.rs
+++ src/lib.rs
@@ -2,37 +2,12 @@ extern crate toml;
 use toml::Value;
 use std::error::Error;
 
-struct Key {
-    pub name: String,
-    pub optional_name: Option<String>
-}
-
-impl Key {
-    pub fn new(name: String) -> Key {
-        Key {
-            name: name,
-            optional_name: None
-        }
-    }
-
-    pub fn new_with_optional_name(name: String, optional_name: String) -> Key {
-        Key {
-            name: name,
-            optional_name: Some(optional_name)
-        }
-    }
-}
-
 pub fn validate(cargo_toml: &str) -> Result<(), Vec<String>> {
     let toml_table = match cargo_toml.parse::<Value>() {
         Ok(table) => table,
         Err(err) => return Err(vec!(err.description().into()))
     };
 
-    let required_keys = vec!(Key::new("description".into()),
-                            Key::new_with_optional_name("license".into(), "license-file".into()),
-                            Key::new("documentation".into()),
-                            Key::new_with_optional_name("homepage".into(), "repository".into()));
     let package_section = toml_table["package"].as_table().unwrap();
 
     let mut errors = vec!();
